8eb06c78ae62316ec715856113135ffd6dae91c5
Rahul Sabharwal <rsabharw@ciena.com>
2016-09-28 22:12:11 +0530

    1. Added Json Files as data source for bookapp
    2. Modified code to return data from json file from MVC web API
    3. Added a new app - SchoolApp for Routing in depth hands on
    4. Added new module, controller, view and link these in MVC app  - using MVC Controller and cshtml files

---
+++ src/AngularHandsOn/wwwroot/js/schoolapp/app.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="templates/home.html" />
+(function () {
+    "use strict";
+    var app = angular.module("schoolBuddies", ['ngRoute']);
+    app.config(['$logProvider', '$routeProvider', configSection]);
+
+    function configSection($logProvider, $routeProvider) {
+
+        $logProvider.debugEnabled(true);
+
+        $routeProvider
+            .when('/',
+            {
+                controller: 'HomeController',
+                controllerAs: 'home',
+                templateurl: '../../js/schoolapp/templates/home.html'
+            });
+    };
+
+}());
+
---
+++ src/AngularHandsOn/wwwroot/js/schoolapp/controllers/HomeController.js
@@ -0,0 +1,15 @@
+﻿(function () {
+
+    angular.module('app')
+        .controller('HomeController', [HomeController]);
+
+    function HomeController(dataService, notifier, $route, $log) {
+
+        var vm = this;
+
+        vm.message = 'Welcome to School Buddy!';
+
+
+    }
+
+}());
