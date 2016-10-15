31c282b3765744f23ef89d6f341cd8100157c2cf
Rahul Sabharwal <rsabharw@ciena.com>
2016-10-16 00:43:39 +0530

    This is start of decorative directives. These directives are created as attributes only - restrict: 'A'
    Added a link function - to add a spacebar support for pausing and playing the video
    link is something that returns a function - it accepts scope variable, element on which it is defined and attributes as array defined in that e;ement
    e.g we added this in video element - el is video element

--- src/AngularHandsOn/wwwroot/js/ngdirectiveapp/app.js
+++ src/AngularHandsOn/wwwroot/js/ngdirectiveapp/app.js
@@ -46,6 +46,23 @@
         }
     });
 
+    angular.module('ngdirective').directive('spacebarSupport', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, el, attrs) {
+                $('body').on('keypress', function (evt) {
+                    var vidEl = el[0];
+                    if (evt.keyCode === 32) {
+                        if (vidEl.paused) {
+                            vidEl.play();
+                        } else {
+                            vidEl.pause();
+                        }
+                    }
+                })
+            }
+        }
+    })
 
     angular.module('ngdirective').directive('userInfoCard', function () {
         return {
