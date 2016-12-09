39762ba2676ece8c85dff52f997b82ab49f82ec2
Sabharwal <rsabharw@ciena.com>
2016-12-09 23:47:31 +0530

    Self Validating Model Example - using IValidatableObject
    When you need to validate one property using one or more properties in the same model.
    For.e.g.let's say you need to validate Property1 which is dependent on value of Property2.
    Then you can use the IValidatableObject.Validate() method to validate the model
    with some custom logic

--- src/AngularHandsOn/Model/SchoolModel.cs
+++ src/AngularHandsOn/Model/SchoolModel.cs
@@ -8,17 +8,33 @@ using System.Threading.Tasks;
 
 namespace AngularHandsOn.Model
 {
-    public class SchoolModel
+    public class SchoolModel : IValidatableObject
     {
         [JsonProperty(PropertyName = "id")]
         public string SchoolId { get; set; }
-        [Required]
+        [Required]        
         [JsonProperty(PropertyName = "name")]
+        [DataType(DataType.Custom,ErrorMessage ="",ErrorMessageResourceName ="")]
         public string Name { get; set; }
         [Required]
         [JsonProperty(PropertyName = "principal")]
         public string Principal { get; set; }
         [JsonProperty(PropertyName = "date")]
         public DateTime Date { get; set; }
+
+        /// <summary>
+        /// When you need to validate one property using one or more properties in the same model.
+        ///For.e.g.let's say you need to validate Property1 which is dependent on value of Property2.
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var pName = new[] { "Name" };
+            if (Name.Contains("&")
+            {
+                yield return new ValidationResult("Name cannot contain '&'", pName);
+            }
+        }
     }
 }
