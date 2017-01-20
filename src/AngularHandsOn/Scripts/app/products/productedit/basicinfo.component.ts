26ba9755365d1f326c9892bb879e33aeb9b9437f
Sabharwal <rsabharw@ciena.com>
2017-01-21 00:34:01 +0530

    Added the form array to add dynamic control in product edit page
    refactored the code to use the form builder and
    used the form array to display the Tags control which are dynamically added
    in the form using the Add Tag button

--- src/AngularHandsOn/Scripts/app/products/productedit/basicinfo.component.ts
+++ src/AngularHandsOn/Scripts/app/products/productedit/basicinfo.component.ts
@@ -1,6 +1,11 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { ValidatorService } from '../../shared/validator.service';
 
 @Component({
     templateUrl: '../../app/products/productedit/basicinfo.component.html'
@@ -8,20 +13,36 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 export class ProductBasicInfoComponent implements OnInit {
     productInfoForm: FormGroup;
     id: number;
+    private sub: Subscription;
     constructor(
         private route: ActivatedRoute,
-        private router: Router, private formBuilder: FormBuilder
-    ) { }
+        private router: Router, private fb: FormBuilder
+    ) {
+        this.route.params.subscribe(
+            params => {
+                let id = +params['id'];
+            }
+        );
+    }
     buildForm() {
-        this.productInfoForm = new FormGroup({
-            productName: this.formBuilder.control(null),
-            productCode: this.formBuilder.control(null),
-            availabilityDate: this.formBuilder.control(null),
-            description: this.formBuilder.control(null)
+        this.productInfoForm = this.fb.group({
+            productName: ['', Validators.required],
+            productCode: ['', Validators.required],
+            rating: ['', ValidatorService.ratingRange(1,5)],
+            availabilityDate: '',
+            description: '',
+            tags: this.fb.array([])
         });
     }
     ngOnInit() {
         this.buildForm();
         this.id = +this.route.snapshot.url[0];
     }
+    get tags(): FormArray {
+        return <FormArray>this.productInfoForm.get('tags');
+    }
+    addTag() {
+        this.tags.push(new FormControl());
+    }
+
 }
