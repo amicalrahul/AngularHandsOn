de479f82c6af62fc281e0a24236fccdee8d8c6ca
Sabharwal <rsabharw@ciena.com>
2017-02-12 22:21:37 +0530

    Updated protractor test cases with page objects and divided Test cases in suites
    To run a particular suite of test cases in protractor, run the below command:
    protractor conf.js --suite=smoke
    To run multiple suite, run the same command with comma separated suites
    protractor conf.js -- suite=smoke, full
    Here, knight button text is not working correctly. Need to fix that test case.

---
+++ src/AngularHandsOn/wwwroot/protractor/smoke/pages/directive.page.js
@@ -0,0 +1,8 @@
+﻿module.exports = function () {
+    this.knightMeButton = element(by.buttonText('Knight Me'));
+    this.name = element(by.binding('name'));
+    this.rank = element(by.binding('rank'));
+    this.updateRank = function () {
+        this.knightMeButton.click();
+    }
+}
