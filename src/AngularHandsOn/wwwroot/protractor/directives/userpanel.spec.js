547aa4e8565ece9b9926e731b327be0242f5c5a2
Sabharwal <rsabharw@ciena.com>
2017-02-12 21:19:50 +0530

    Added End to End test with Protractor
    Installing Protractor – open command line and run below commands
    	1. npm  install –g protractor => install protractor
    	2. npm i -g webdriver-manager@~10.2.7  => installs selenium wedriver (version 10.2.7 as later version requires node 6.0 and higher version
    	3. webdriver-manager update => updates the selenium driver to run protractor test cases
    		https://github.com/angular/webdriver-manager/issues/170 - this url describe the webdriver issue
    	4. To start run below commands:
    		webdriver-manager start
    	then
    		protractor conf.js

---
+++ src/AngularHandsOn/wwwroot/protractor/directives/userpanel.spec.js
@@ -0,0 +1,24 @@
+﻿describe('angular directive app test', function () {
+    var name;
+    beforeEach(function () {
+        browser.get('http://localhost:44312/Home/NgDirective#/');
+        var firstElement = element.all(by.binding('name')).first();
+        firstElement.getText().then(function (text) {
+            name = text;
+        });
+
+        firstElement.click();
+
+        browser.waitForAngular();
+    });
+    it('Should navigate to the details page', function () {
+        var header = element(by.binding('name'));
+
+        expect(header.getText()).toMatch('Luke Skywalker');
+    });
+
+    //it('Should update the url', function () {
+    //    expect(browser.getCurrentUrl()).toMatch('EventRatings/');
+    //});
+
+});
