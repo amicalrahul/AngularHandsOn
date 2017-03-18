ebfa564f519928a54885188db81d811e14c69051
RahulS <amicalrahul@gmail.com>
2017-03-18 23:58:15 +0530

    Moved the Entities to Domain Project and DbContext to Data project
    Added separate projects for domain entities and dbcontext.
    As a best practice we should keep the domain/Entity Model separate

--- src/AngularHandsOn.Domain/School.cs
+++ src/AngularHandsOn.Domain/School.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
 
 namespace AngularHandsOn.Entities
 {
