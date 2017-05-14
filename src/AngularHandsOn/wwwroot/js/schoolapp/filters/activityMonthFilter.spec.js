31eef484f01f171b3025cc4a8e2dde18f7d3cf7f
amicalrahul <amicalrahul@gmail.com>
2017-05-15 00:52:06 +0530

    Added unit test cases for custom filters
    using inject-$filter angular service to create filter mock instance  just like
    $controller service is used to create controller instance

---
+++ src/AngularHandsOn/wwwroot/js/schoolapp/filters/activityMonthFilter.spec.js
@@ -0,0 +1,18 @@
+﻿describe('activity Month Filter', function () {
+  
+    var activityMonthFilter;
+    var activities = mockData.getMockActivities();
+
+    beforeEach(module('schoolBuddies'));
+
+    beforeEach(inject(function (_$filter_) {
+        activityMonthFilter = _$filter_('activityMonthFilter');
+    }));
+
+    it('should return all activities if no filterMonth is provided', function () {
+        expect(activityMonthFilter(activities)).to.be.equal(activities);
+    });
+    it('should return matching activities if filterMonth is provided', function () {
+        expect(activityMonthFilter(activities, 10)[0]).to.be.equal(activities[0]);
+    });
+});
--- src/AngularHandsOn/wwwroot/js/test-helpers/mock-data.js
+++ src/AngularHandsOn/wwwroot/js/test-helpers/mock-data.js
@@ -3,7 +3,8 @@ var mockData = (function () {
     return {
         getMockBook: getMockBook,
         getMockStates: getMockStates,
-        getMockReaders: getMockReaders
+        getMockReaders: getMockReaders,
+        getMockActivities: getMockActivities
     };
 
     function getMockStates() {
@@ -37,6 +38,14 @@ var mockData = (function () {
             { "book_id": 4, "title": "The Hobbit", "author": "J. R. R. Tolkien", "year_published": 1937 },
             { "book_id": 5, "title": "Curious George", "author": "H. A. Rey", "year_published": 1941 },
             { "book_id": 6, "title": "Alice's Adventures in Wonderland", "author": "Lewis Carroll", "year_published": 1865 }
+        ];
+    }
+
+    function getMockActivities() {        
+        return [
+            { "activity_id": 1, "name": "Museum Field Trip", "date": "2015-10-01T16:00:00.000Z", "classroom_id": 1, "school_id": 1 },
+            { "activity_id": 2, "name": "Book Fair", "date": "2015-11-06T16:00:00.000Z", "classroom_id": 2, "school_id": 1 },
+            { "activity_id": 3, "name": "Petting Zoo Visit", "date": "2015-12-19T16:00:00.000Z", "classroom_id": 3, "school_id": 2 }
         ];
     }
 })();
