c3e5ec04ad6d4e1bb8064c05d00f860f936cce9c
amicalrahul <amicalrahul@gmail.com>
2017-05-15 00:20:46 +0530

    Added angular directuve unit testing
    For directives unit testing, few things were followed:
    1. $rootScope is used to pass the values to the directives
    2. $compile service is used to compile the directive into HTML
    3. $rootScope.$digest() is called to apply the directive bindings to the HTML.
    4. Also, directive is also supporting transclusion and with that , the text and edit book hyperlink
    are displayed in a single line (just like earlier representation and without transclusion book title and edit link will
    come in separate lines)

---
+++ src/AngularHandsOn/wwwroot/js/bookapp_updated/directives/bookTitle.directive.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var app = angular.module("app.directive");
+
+    app.directive("bookTitle", [bookTitle]);
+
+    function bookTitle() {
+        return {
+            restrict: 'E',
+            scope: {
+                book:'=book'
+            },
+            transclude: true,
+            template: '<div>{{ book.title }} - {{ book.author }}<span ng-transclude></span></div>'
+        }
+
+    }
+})();
---
+++ src/AngularHandsOn/wwwroot/js/bookapp_updated/directives/bookTitle.directive.spec.js
@@ -0,0 +1,20 @@
+﻿describe("BookTitle Directive", function () {
+    beforeEach(module('bookapp'));
+    var $compile;
+    var $rootScope;
+    beforeEach(inject(function (_$compile_, _$rootScope_, _$httpBackend_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        _$httpBackend_.when('GET', '../../js/bookapp_updated/home.html').respond('');
+    }));
+
+    var books = mockData.getMockBook();
+
+    it("should display book title as html", function () {
+        $rootScope.book = books[0];
+        var expectedHtml = '<div class="ng-binding">Goodnight Moon - Margaret Wise Brown<span ng-transclude=""></span></div>';
+        var element = $compile('<book-title book="book"></book-title>')($rootScope);
+        $rootScope.$digest();
+        expect(element.html()).to.be.equal(expectedHtml);
+    });
+});
